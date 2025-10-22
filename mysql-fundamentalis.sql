17c46ba2854369bca78bd3541184f2bcc6286013
Bryan <bryan.alexander.glot.fong@alu.ceacfp.es>
2025-10-22 13:57:18 +0200

    versión 0.4(22-10-2025)
    Versión mejorada con la addición de clave foránea y algunas ejemplos tablas-columnas para mejor copiado y pegado

--- mysql-fundamentalis.sql
+++ mysql-fundamentalis.sql
@@ -22,7 +22,7 @@ SHOW TABLES;
 -- Create DATABASE--
 ------------------
 
-CREATE DATABASE empresadam;
+CREATE DATABASE basededatos;
 
 ---------------
 --Create TABLE--
@@ -32,7 +32,8 @@ CREATE TABLE clientes (
   dni VARCHAR(9),
   nombres VARCHAR(50),
   apellidos VARCHAR(255),
-  email VARCHAR(100)
+  email VARCHAR(100),
+  PRIMARY KEY("Identificador" AUTOINCREMENT)
 );
 
 CREATE TABLE productos (
@@ -67,34 +68,44 @@ VALUES(
 ------------
 SELECT * FROM clientes;
 
---------------
--- Describe --
---------------
+--------------------------------------------------------
+-- Describe(Muestra las propiedades de clientes) --
+--------------------------------------------------------
 DESCRIBE Clientes;
 
 -----------------
 -- PARA AÑADIR COLUMNA--
 -----------------
-ALTER TABLE clientes
-ADD COLUMN direccion VARCHAR(255);
+ALTER TABLE tabla
+ADD COLUMN columna VARCHAR(255);
 
 -----------------
 -- PARA ELIMINAR COLUMNA --
 -----------------
-ALTER TABLE clientes
-DROP COLUMN direccion;
+ALTER TABLE tabla
+DROP COLUMN columna;
 
 ----------------------------------
    -- PARA REINICIAR TABLA --
 ----------------------------------
-TRUNCATE TABLE clientes; -- resetea la tabla pero no se carga
+TRUNCATE TABLE tabla; -- resetea la tabla pero no se carga
 
 ---------------------------------------------------
 Para alterar clientes y añadir una clave primaria
 ---------------------------------------------------
-ALTER TABLE clientes
+ALTER TABLE tabla
 ADD COLUMN identificador INT AUTO_INCREMENT PRIMARY KEY FIRST;
 
+---------------------------------------------------
+Para añadir una clave ajena ("foreign key"/fk)
+---------------------------------------------------
+
+ALTER TABLE email                                       --Altera la tabla de email
+ADD CONSTRAINT fk_emails_personas                        --Crea una restricción con este nombre
+FOREIGN KEY (persona)                                    --Creamos una clave hacia persona
+REFERENCES personas(identificador)                       --que referencia el identificador 
+ON DELETE CASCADE                                        --Cuando elimines, cascada
+ON UPDATE CASCADE;                                       --Cuando actualices, cascada
 ---------------------------------------------------
 MUCHO CUIDADOMUCHO CUIDADOMUCHO CUIDADOMUCHO CUIDADO
 ---------------------------------------------------
@@ -147,3 +158,46 @@ ALTER TABLE clientes
 
 
 
+------------------------------------------------------------------------------------------------------------------
+-C                          R                          U                      D 
+
+----------------------------------------------------------------------------------------------------------------
+--create
+INSERT INTO clientes VALUES(
+ NULL,
+ 'Bryan',
+ 'Glot Fong',
+ 'info@gmail.com'
+);
+
+--read
+SELECT * FROM clientes;
+
+--update
+UPDATE clientes
+SET email = 'nuevainfo@gmail.com'
+WHERE Identificador = 1;
+
+--delete
+DELETE FROM clientes
+WHERE Identificador = 1;
+
+
+------------------------------------------------------------------------------------------------------------------
+
+                        p             y             t             h           o           n
+                                                          +
+                                            s             q           l
+                        
+------------------------------------------------------------------------------------------------------------------
+-DOC IMPORTANTE:
+------------------------------
+- TRES COMILLAS DOBLES DE PYTHON
+- COMILLAS SIMPLES DE SQL
+------------------------------
+
+cursor.execute("""
+ INSERT INTO clientes VALUES(
+   NULL,'"""+nombre+"""','"""+apellidos+"""','"""+email+"""'
+ );
+""")

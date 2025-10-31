4dc739719678c2d597fe5a1fb10100ff9b4987cc
Bryan <bryanglot@hotmail.com>
2025-10-31 08:46:07 +0100

    Versión de 29-10-2025
    e

--- mysql-fundamentalis.sql
+++ mysql-fundamentalis.sql
@@ -6,6 +6,9 @@
 ------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------
 
+
+Siempre recordar cerrar las comas, llaves, y entre cosas (te veo archivo.close())
+------------------------------------------------------------------------------------------------------------------
 En terminal:
 Para Copiar => Ctrl + Mayusculas + C
 
@@ -157,6 +160,38 @@ ALTER TABLE clientes
   );
 
 
+------------------------------------------------------------------------------------------------------------
+Para crear un usuario
+------------------------------------------------------------------------------------------------------------
+
+-- crea usuario nuevo con contraseña
+-- creamos el nombre de usuario que queramos
+CREATE USER 
+'[tunombredeusuario]'@'[localhost]' 
+IDENTIFIED  BY '[tucontraseña]';
+
+-- permite acceso a ese usuario
+GRANT USAGE ON *.* TO '[tunombredeusuario]'@'[tuservidor]';
+--[tuservidor] == localhost
+-- La contraseña puede requerir Mayus, minus, carácteres especiales,etc
+
+-- quitale todos los limites que tenga
+ALTER USER '[tunombredeusuario]'@'[tuservidor]' 
+REQUIRE NONE 
+WITH MAX_QUERIES_PER_HOUR 0 
+MAX_CONNECTIONS_PER_HOUR 0 
+MAX_UPDATES_PER_HOUR 0 
+MAX_USER_CONNECTIONS 0;
+
+-- dale acceso a la base de datos
+GRANT ALL PRIVILEGES ON `[tubasededatos]`.* 
+TO '[tunombredeusuario]'@'[tuservidor]';
+
+-- recarga la tabla de privilegios
+FLUSH PRIVILEGES;
+
+
+
 
 ------------------------------------------------------------------------------------------------------------------
 -C                          R                          U                      D 
@@ -201,3 +236,6 @@ cursor.execute("""
    NULL,'"""+nombre+"""','"""+apellidos+"""','"""+email+"""'
  );
 """)
+
+
+
